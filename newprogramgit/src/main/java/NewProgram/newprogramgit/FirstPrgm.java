c6e13867f6ac3b23adad6e3723f3eaf15cc7391c
jeevitha123-test <jeevithareddy.reddy9@gmail.com>
2024-09-12 11:23:17 +0530

    first prgm

---
+++ newprogramgit/src/main/java/NewProgram/newprogramgit/FirstPrgm.java
@@ -0,0 +1,11 @@
+package NewProgram.newprogramgit;
+
+public class FirstPrgm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("first program for git");
+
+	}
+
+}
---
+++ newprogramgit/src/test/java/NewProgram/newprogramgit/AppTest.java
@@ -0,0 +1,19 @@
+package NewProgram.newprogramgit;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
