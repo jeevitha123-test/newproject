f71cbbcf6a7a4e827e69fd8518b49ed2ecd87f28
jeevitha123-test <jeevithareddy.reddy9@gmail.com>
2024-09-13 10:51:56 +0530

    abcdef

---
+++ newprogramgit/src/test/java/NewProgram/newprogramgit/testingfolder.java
@@ -0,0 +1,11 @@
+package NewProgram.newprogramgit;
+
+public class testingfolder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("abcdefghijkl");
+
+	}
+
+}
