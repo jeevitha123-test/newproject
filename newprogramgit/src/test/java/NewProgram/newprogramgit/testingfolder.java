9ee927748d3661643f4fa15d321d4da6dee1b49b
jeevitha123-test <jeevithareddy.reddy9@gmail.com>
2024-09-13 10:56:13 +0530

    passed second statement

--- newprogramgit/src/test/java/NewProgram/newprogramgit/testingfolder.java
+++ newprogramgit/src/test/java/NewProgram/newprogramgit/testingfolder.java
@@ -5,6 +5,8 @@ public class testingfolder {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("abcdefghijkl");
+		System.out.println("testing proccess");
+		System.out.println("testing passed");
 
 	}
 
